9fbd4b9374aae1f6c7de3299c62782b70bb73f80
vivi2004 <vivekpurbey586@gmail.com>
2025-12-13 08:35:43 +0530

    folder structure allign

---
+++ tasks-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count ] = useState(0)
+
+  return (
+    <>
+     <h1 className=' bg-amber-400'>It's me chutiye</h1>
+    </>
+  )
+}
+
+export default App
